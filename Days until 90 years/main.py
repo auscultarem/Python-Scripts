519cb68e64495e22b94b97a1f9bf96857514b633
Jesus Castellanos <auscultarem@gmail.com>
2021-09-03 17:39:05 -0500

    Days until 90 years exercise

---
+++ Days until 90 years/main.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+Age90 = 90
+
+rest_of_life = (90-(int(age)))
+#print(type(rest_of_life))
+days = rest_of_life * 365
+weeks = rest_of_life * 52
+months = rest_of_life * 12
+print(f"You have is {rest_of_life} years or {days} days or {weeks} weeks or {months} months until you reach 90 years old")
