92d5e4036281f4759946992308f0b206b789d504
Jesus Castellanos <auscultarem@gmail.com>
2021-09-16 21:27:31 -0500

    I create functions to improve the project

--- Guess the number/main.py
+++ Guess the number/main.py
@@ -12,33 +12,40 @@ from art import logo
 from replit import clear
 should_continue = True
 
+def game(lives, guess_number):
+	counter = lives	
+	while counter > 0:
+			variable = int(input(f"You have {counter} lifes ,Guess the number: "))
+			if variable == guess_number:
+						print("You win!!")
+						return False
+			elif variable < guess_number:
+						counter -= 1
+						print("Too Low")	
+			elif variable > guess_number:
+						print("Too High")
+						counter -= 1
+	if counter == 0:
+		print("You loose")
+	
+	 
 while should_continue:
 	clear()
 	print(logo)
 	guess_number = random.randint(0, 100)
 	print("Guess the number from 0 to 100.")
 	dificulty = input(("Choose a dificulty. Type 'easy' or 'hard' "))
-	variable = 0
-
+	lives_easy = 10
+	lives_hard = 5
+	
 	if dificulty == 'easy':
-		counter_easy = 10
-		while counter_easy > 0:
-			variable = int(input(f"You have{counter_easy} lifes ,Guess the number: "))
-			if variable == guess_number:
-				counter_easy = 0
-				should_continue = False
-				print("You win!!")
-			elif variable < guess_number:
-				counter_easy -= 1
-				print("Too Low")	
-			elif variable > guess_number:
-				print("Too High")
-				counter_easy -=1
-			elif counter_easy == 0:
-				should_continue = False 	
+		counter = lives_easy
+		should_continue = game(counter, guess_number) 	
 
 	elif dificulty == 'hard':
-		counter_hard = 5
-		should_continue = False
+		counter = lives_hard
+		should_continue = game(counter, guess_number)
 	else:
-		print("type a correct dificulty")		
+		print('Elige una opcion correcta')
+
+			
